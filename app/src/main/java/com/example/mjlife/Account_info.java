a0b65702506713d386e20553e80bbcdee064fc38
dudgk <dudgkr2014@naver.com>
2020-12-18 23:18:42 +0900

    younghak

--- app/src/main/java/com/example/mjlife/Account_info.java
+++ app/src/main/java/com/example/mjlife/Account_info.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Pattern;
+
 public class Account_info extends AppCompatActivity {
 
     EditText telInput, emailInput, passInput, passCheckInput;
@@ -37,19 +39,32 @@ public class Account_info extends AppCompatActivity {
                 passGet = passInput.getText().toString();
                 passCheckGet = passCheckInput.getText().toString();
 
-                if(telGet.equals("") || emailGet.equals("") || passGet.equals("") || passCheckGet.equals("")){
+                if (telGet.equals("") || emailGet.equals("") || passGet.equals("") || passCheckGet.equals("")) {
                     Toast.makeText(Account_info.this, "모든 내용을 입력해주세요!", Toast.LENGTH_SHORT).show();
-                }else{
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(Account_info.this);
-                    dlg.setTitle("수정완료");
-                    dlg.setMessage("수정이 완료되었습니다.");
-                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ;
+
+                } else {
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailGet).matches()){
+                        Toast.makeText(Account_info.this, "이메일 형식에 맞게 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    } else if(!Pattern.matches("^[0-9].{8,11}$", telGet)){
+                        Toast.makeText(Account_info.this, "전화번호를 숫자로만 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    } else if(!Pattern.matches("^[a-zA-Z0-9].{8,16}$", telGet)){
+                        Toast.makeText(Account_info.this, "비밀번호는 숫자와 영문자를 합쳐서 8~16자로 입력하세요.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if(passGet.equals(passCheckGet)) {
+                            AlertDialog.Builder dlg = new AlertDialog.Builder(Account_info.this);
+                            dlg.setTitle("수정완료");
+                            dlg.setMessage("수정이 완료되었습니다.");
+                            dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ;
+                                }
+                            });
+                            dlg.show();
+                        }else{
+                            Toast.makeText(Account_info.this, "비밀번호를 동일하게 입력했는지 확인하세요.", Toast.LENGTH_SHORT).show();
                         }
-                    });
-                    dlg.show();
+                    }
                 }
             }
         });
